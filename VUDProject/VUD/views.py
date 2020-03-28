709b511458acfe211d5e4b1ab6c480ca44de7b4d
TaTRaTa <cvetomir.defyy@gmail.com>
2020-03-29 01:04:12 +0200

    set up static dirs as well as  bootstrap and jquery files are added

--- VUDProject/VUD/views.py
+++ VUDProject/VUD/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse
 # Create your views here.
 
 def Home(request):
-    return HttpResponse('Home')
+    return render(request, "vud/index.html", )
